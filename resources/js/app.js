ba220fc857dc291de9e78f36eaa06b719fb50ece
Alejandro Mendez <amendez@buk.cl>
2024-01-25 18:38:21 -0300

    add template oneui

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,27 @@
 import './bootstrap';
-import '../css/app.css';
+
+import './assets/scss/main.scss';
+import './assets/scss/oneui/themes/amethyst.scss';
+import './assets/scss/oneui/themes/city.scss';
+import './assets/scss/oneui/themes/flat.scss';
+import './assets/scss/oneui/themes/modern.scss';
+import './assets/scss/oneui/themes/smooth.scss';
+
 
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
+import { createPinia } from "pinia";
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+
+// Template components
+import BaseBlock from "./Components/BaseBlock.vue";
+import BaseBackground from "./Components/BaseBackground.vue";
+import BasePageHeading from "./Components/BasePageHeading.vue";
+
+// Bootstrap framework
+import * as bootstrap from "bootstrap";
+window.bootstrap = bootstrap;
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -13,6 +30,10 @@ createInertiaApp({
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
+            .component("BaseBlock", BaseBlock)
+            .component("BaseBackground", BaseBackground)
+            .component("BasePageHeading", BasePageHeading)
+            .use(createPinia())
             .use(plugin)
             .use(ZiggyVue)
             .mount(el);
--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,10 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    if (auth()->check()) {
+        return redirect()->route('dashboard');
+    }
+    return redirect()->route('login');
 });
 
 Route::get('/dashboard', function () {
